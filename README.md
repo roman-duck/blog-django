39fb56729b8b6340bee2560029ed9f5f667d9726
Yellow Duck <mvch@triangle.software>
2025-09-20 00:08:57 +0000

    canonic url

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # A sample Django Project
+
+## Run server
+
+```bash
+python manage.py runserver
+```
+## Make migration
+```bash
+python manage.py makemigrations blog
+```
+## Run migration
+```bash
+python manage.py migrate
+
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -12,7 +13,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='published')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -37,3 +38,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse ('blog:post_detail', args=[self.published.year,
+                                                  self.published.month,
+                                                  self.published.day,
+                                                  self.slug])
+    
