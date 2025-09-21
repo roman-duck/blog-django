fe68a2a5e51c91f3d20e1ad7ac1713022b87adc3
Yellow Duck <mvch@triangle.software>
2025-09-21 20:16:02 +0000

    paginator

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+DEFAULT_PAGE_NUMBER = 1
 
 def post_list(request):
-    posts = Post.published_posts.all()
-    return render(request, 'blog/post/list.html', {'posts': posts}) 
+    post_list = Post.published_posts.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', DEFAULT_PAGE_NUMBER)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(DEFAULT_PAGE_NUMBER)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
