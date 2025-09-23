7bb4b3795446e967ec9b7f76923314417cf4d2a7
Yellow Duck <mvch@triangle.software>
2025-09-23 12:35:41 +0000

    sitemaps

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('sitemap.xml', sitemap, {'sitemaps': {'posts': PostSitemap}}, name='django.contrib.sitemaps.views.sitemap'),
 ]
